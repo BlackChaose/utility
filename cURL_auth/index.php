d4af8e365b696e1313eb5f9360a52076590d567a
nik <nik@0x4e494b.ru>
2019-08-26 03:27:03 +0300

    added comment

--- cURL_auth/index.php
+++ cURL_auth/index.php
@@ -107,6 +107,9 @@ foreach ($items as $item) {
 $img=base64_encode(file_get_contents(URLOPENLK.$photo_src));
 $src = 'data: '.mime_content_type(URLOPENLK.$photo_src).';base64,'.$img;
 //TODO - отобразить отпарсенную таблицу с результатми тестов + отдельную таблицу с рекуомендуемыми учебными ресурсами.
+//ATTENTION  see https://docs.mitmproxy.org/stable/tools-mitmdump/
+//ATTENTION see https://phpclub.ru/talk/threads/%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-curl-%D0%B4%D0%BB%D1%8F-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8.59542/
+//ATTENTION see https://unix.stackexchange.com/questions/103037/what-tool-can-i-use-to-sniff-http-https-traffic/103039
 ?>
 <!doctype html>
 <html lang="ru">
