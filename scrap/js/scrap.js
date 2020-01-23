6bae21237833a012e33eca74b8c48bab829c6ed7
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2020-01-23 14:44:45 +0300

    add(2019): add data by 2019

--- scrap/js/scrap.js
+++ scrap/js/scrap.js
@@ -1,8 +1,9 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-let year = [1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018];
+//let year = [1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019];
 //let year = [1998,1999,2000];
+let year = [2019];
 let month = [1,2,3,4,5,6,7,8,9,10,11,12];
 let dataT=[];	
 let parse = function(yr, mnt){return new Promise((resolve, reject)=>{
@@ -66,7 +67,7 @@ Promise.all(arrPromises)
 
 	const fs = require('fs');
 	buf.forEach(element => {	
-		fs.appendFile("out.txt",`(${element.year} - ${element.month} ): ${element.record}`,function(err){if(err){return console.error(err);}
+		fs.appendFile("out2.txt",`(${element.year} - ${element.month} ): ${element.record}`,function(err){if(err){return console.error(err);}
 		console.warn("The file was changed!");
 			});}
 	);
