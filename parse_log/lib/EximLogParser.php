4c0ec513f975327048198aa5d9cbd4dbdb01f985
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2020-03-05 12:48:45 +0300

    add(class): EximLogParser, move parsed 'sends emails' list to output file

---
+++ parse_log/lib/EximLogParser.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * User: nikita.s.kalitin@gmail.com
+ * Date: 05.03.20
+ * Time: 9:51
+ */
+
+namespace Parsers;
+
+use Tightenco\Collect;
+
+class EximLogParser
+{
+    /**
+     * @param $input_file       file contains log info
+     * @param $output_file      parse result file
+     * @param $parameters   ['filters'=>['<=','(=','=>','->','>>','*>','**','=='], 'exclude_mails'=>['mail@mail1.example','mail@mail2.example']]
+     *                      <=      message arrival
+     *                      (=     message fakereject
+     *                      =>     normal message delivery
+     *                      ->     additional address in same delivery
+     *                      >>     cutthrough message delivery
+     *                      *>     delivery suppressed by -N
+     *                      **     delivery failed; address bounced
+     *                      ==     delivery deferred; temporary problem
+     *                      info from http://www.exim.org/exim-html-current/doc/html/spec_html/ch-log_files.html
+     * @throws \Exception
+     */
+    public static function parse($input_file, $output_file, $parameters)
+    {
+
+        $res = [];
+        $datalog = [];
+        $file = fopen($input_file, "r");
+        while (!feof($file)) {
+            $res[] = explode(' ', fgets($file));
+        }
+        fclose($file);
+        foreach ($res as $el) {
+            $datalog[] = [
+                'day' => $el[0] ?? null,
+                'time' => $el[1] ?? null,
+                'id-message' => $el[2] ?? null,
+                'direct' => $el[3] ?? null,
+                'info' => $el[4] ?? null,
+                'reserv' => $el,
+            ];
+        }
+        $collection = collect($datalog);
+
+        if (!empty($parameters) && !empty($parameters['direct_filters'])) {
+            $filtered = $collection->filter(function ($val, $key) use ($parameters) {
+                $direct_filters_collect = collect($parameters['direct_filters']);
+                $info_exclude_mails = collect($parameters['exclude_mails']);
+                if ($direct_filters_collect->contains($val['direct']) && (!empty($val['info']) && !$info_exclude_mails->contains($val['info']))) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        } else {
+            throw(new \Exception('Filter constant not set!'));
+        }
+
+        $grouped = $filtered->groupBy('id-message');
+        $sorted = $grouped->sortBy('id-message');
+
+        $out = fopen($output_file, 'aw');
+
+        $sorted->each(function ($item) use ($out) {
+            fwrite($out, $item->pluck('info')[0] . "\n");
+        });
+
+        fwrite($out, 'counts: '.$sorted->count()."\n");
+        fwrite($out, 'file_created_at: '.date('Y-M-D H:m:s'));
+
+        fclose($out);
+    }
+}
--- parse_log/pl.php
+++ parse_log/pl.php
@@ -5,26 +5,25 @@
  * Parser for get data about send mails from /var/log/exim4/mainlog
  * type in console php pl.php -s <soruce.file> -d <output.file>
  */
-//todo add check timeout&message error in log etc.
 namespace ParserLog;
 
 require_once 'vendor/autoload.php';
 require_once 'env.php';
 require_once 'constants.php';
-
+use Parsers\EximLogParser;
 use Tightenco\Collect;
 try {
     $options = getopt("s::d::hv::");
-//    print_r($options); die;
+
     if(isset($options["h"])){
-        echo ESC."[32m  "."-s <path to source> -d <path to dst>"."\e[39m\n";
-        echo ESC."[32m  "."-v <show version>"."\e[39m\n";
-        echo ESC."[32m  "."-h <show help>"."\e[39m\n";
+        echo GREEN_FONT."-s <path to source> -d <path to dst>".DEFATTR."\n";
+        echo GREEN_FONT."-v <show version>".DEFATTR."\n";
+        echo GREEN_FONT."-h <show help>".DEFATTR."\n";
         die;
     }
 
     if (isset($options["v"])) {
-        echo VIOLET_FONT."m version 0.1 03-03-2020 ".DEFATTR."\n";
+        echo VIOLET_FONT."version 0.1 03-03-2020 ".DEFATTR."\n";
         die;
     }
 
@@ -49,72 +48,11 @@ try {
         throw(new \Exception('destination file are exist! use -h for help!'));
     }
 
-    echo "\033[92m source:  " . $options['s'] . "\e[39m\n";
-    echo "\033[93m destination:  " . $options['d'] . "\e[39m\n";
-
-    $res = [];
-    $datalog = [];
-    $file = fopen($file_path_src, "r");
-    while (!feof($file)) {
-        $res[] = explode(' ', fgets($file));
-    }
-    fclose($file);
-    foreach ($res as $el) {
-        $datalog[] = [
-            'day' => $el[0] ?? null,
-            'time' => $el[1] ?? null,
-            'id-message' => $el[2] ?? null,
-            'direct' => $el[3] ?? null,
-            'info' => $el[4] ?? null,
-            'reserv' => $el,
-        ];
-    }
-    $collection = collect($datalog);
-
-    /**
-     * info from http://www.exim.org/exim-html-current/doc/html/spec_html/ch-log_files.html
-     *  <=      message arrival
-     *  (=     message fakereject
-     *  =>     normal message delivery
-     *  ->     additional address in same delivery
-     *  >>     cutthrough message delivery
-     *  *>     delivery suppressed by -N
-     *  **     delivery failed; address bounced
-     *  ==     delivery deferred; temporary problem
-     */
-
-    if (FILTER_ADDRESS_SERVER !== '' && FILTER_ADDRESS_FROM !== '') {
-        $filtered = $collection->filter(function ($val, $key) {
-            if (($val['direct'] === '=>' || $val['direct'] === "Completed\n") && (trim($val['info']) !== FILTER_ADDRESS_FROM && trim($val['info']) !== FILTER_ADDRESS_SERVER)) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    } else {
-        throw(new \Exception('Filter constant not set!'));
-    }
-
-
-    $grouped = $filtered->groupBy('id-message');
-    $sorted = $grouped->sortBy('id-message');
-
-    $sorted2 = $sorted->filter(function ($val) {
-        return $val->count() >= 2;
-    });
-    $out = fopen($file_path_dest, 'aw');
-    $sorted2->each(function ($item) use ($out) {
-        //echo $item->pluck('info')[0];
-        //echo "\n";
-        echo GREEN_FONT.".".DEFATTR;
-
-        fwrite($out, $item->pluck('info')[0] . "\n");
-    });
-    echo "\n";
-    fclose($out);
-    echo "\033[92m count:  " . $sorted2->count() . "\e[39m\n";
+    echo YELLOW_FONT."source:  ".$options['s'].DEFATTR."\n";
+    echo YELLOW_FONT."destination:  ".$options['d'].DEFATTR."\n";
+    EximLogParser::parse($file_path_src,$file_path_dest,['direct_filters'=>["=>","Completed\n"],'exclude_mails'=>[FILTER_ADDRESS_FROM, FILTER_ADDRESS_SERVER]]);
 }
 catch(\Exception $e){
-    echo "\033[31m  ERR!  <".$e->getMessage().">\e[39m\n";
+    echo RED_FONT."ERR!  <".$e->getMessage().">\e[39m\n";
     die;
 }
